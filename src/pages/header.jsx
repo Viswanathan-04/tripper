4b78707389d217c319724f627e89107f856e10d9
Viswanathan <vish2004k@gmail.com>
2023-08-28 21:56:19 +0530

    modified:   src/pages/header.jsx

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -6,21 +6,21 @@ function Header()
         <>
             <div className="navbar">
                 <div className="logo">
-                    <a href='/' className="logo-text"><span className="logo-text-span">T</span>rippe<span className="logo-text-span">r</span></a>
+                    <a href='/tripper' className="logo-text"><span className="logo-text-span">T</span>rippe<span className="logo-text-span">r</span></a>
                 </div>
                 <div className='links'>
                     <ul>
                         <li>
-                            <a href='/' id='head-a' >Home</a>
-                            <a href='/' id='head-a'>About</a>
-                            <a href='/' id='head-a'>Services</a>
-                            <a href='/' id='head-a'>Explore</a>
-                            <a href='/' id='head-a'>Contact</a>
+                            <a href='/tripper' id='head-a' >Home</a>
+                            <a href='/tripper' id='head-a'>About</a>
+                            <a href='/tripper' id='head-a'>Services</a>
+                            <a href='/tripper' id='head-a'>Explore</a>
+                            <a href='/tripper' id='head-a'>Contact</a>
                         </li>
                     </ul>
                 </div>
                 <div className='search'>
-                    <a className='login-btn' href='/'>Login</a>
+                    <a className='login-btn' href='/tripper'>Login</a>
                 </div>
             </div>
         </>
